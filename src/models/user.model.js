8b7dcd1ebaa201071f04fb66c5bfce2e6d501da9
Satyam <sm2003satyam@gmail.com>
2024-12-12 22:13:09 +0530

    created user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,95 @@
+import mongoose, { Schema } from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, "username is required"],
+        unique: [true, "username is already taken"],
+        lowercase: [true, "username should be in lowercase"],
+        trim: true,  // this will automatically remove extra spaces at starting and ending of a string , ONLY WORKS FOR A STRING
+        index: true  // improve query performance and more, but you have to trade off with storage space, slower write operations, etc.
+    },
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        unique: [true, "email already exists"],
+        lowercase: [true, "email should be in lowercase"],
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: [true, "fullname is required"],
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String,  // cloudinary url
+        required: true
+    },
+    coverImage: {
+        type: String    // cloudinary url
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, "password is required"],
+    },
+    refreshToken: {
+        type: String
+    }
+}, {timestamps: true})
+
+// pre is a function that is available on schemas and this function is used to write middlewares, this function takes 2 parameters, first the hook or middleware type that specifies the lifecycle event of the model on which the middleware will be triggered.
+// this will encrypt the password before saving it in the database
+userSchema.pre("save", function(next) {
+    if(!this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+// method to check the password correct or not
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+// method to generate jwt access token
+userSchema.methods.generateAccessToken = function() {
+    jwt.sign(
+        // payloads
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,  // secret key
+        // expiry object
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+// method to generate jwt refresh token
+userSchema.methods.generateRefreshToken = function() {
+    jwt.sign(
+        // payloads
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,  // secret key
+        // expiry object
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,7 +1,38 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const UserModel = new mongoose.Schema({
+const videoSchema = new Schema({
+    videoFile: {
+        type: String,  // we can store the video file in the db, but it is not a good practice, so we store the url of the video file that we get from cloudinary
+        required: true
+    },
+    thumbnail: {
+        type: String,  // url of thumbnail
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,  // ye bhi hume cloudinary se mill jayega
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true })
 
-}, {timestamps: true})
-
-export { UserModel }
+export const Video = mongoose.model("Video", videoSchema);

045c5917c731a62767c1e857f4366d790497a2bd
Satyam <sm2003satyam@gmail.com>
2024-12-26 21:30:35 +0530

    Add ProtectedRoute component and implement routing for Home, Login, and Register pages

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import App from "./App.jsx"
+import Home from './pages/Home.jsx'
+import Login from './pages/Login'
+import Register from './pages/Register.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "",
+        element: (
+          <ProtectedRoute>
+            <Home/>
+          </ProtectedRoute>
+        )
+      },
+      {
+        path: "/login",
+        element: <Login/>
+      },
+      {
+        path: "/register",
+        element: <Register/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
